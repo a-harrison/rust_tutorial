1d4e73df0343d423871d3b16b9aa1278707b1b4f
Adam Harrison <adamaharrison@gmail.com>
2019-04-10 17:38:00 -0700

    Adding more.

---
+++ 6_enums/enum.rs
@@ -0,0 +1,42 @@
+fn main() {
+    enum IpAddrKind{
+        V4,
+        V6
+    }
+
+    struct IpAddr {
+        kind: IpAddrKind,
+        address: String,
+    }
+
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+
+    let loopback = IpAddr {
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
+
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
+
+    // ...................................... 
+
+    enum Message {
+        Quit,
+        Move { x: i32, y: i32 },
+        Write(String),
+        ChangeColor(i32, i32, i32),
+    }
+
+    impl Message {
+        fn call(&self) {
+            // method body would be defined here
+        }
+    }
+
+    let m = Message::Write(String::from("hello"));
+    m.call();
+}
