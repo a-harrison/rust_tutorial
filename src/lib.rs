a0308537fa895d111bf2bbc04547bdf4fe5049f0
Adam Harrison <adamaharrison@gmail.com>
2019-07-22 18:16:32 -0700

    Completed 12.1 through 12.5

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #![allow(dead_code)]
 
 // Include which modules to include in the library
-pub mod ch5_structs;
-pub mod ch9_errors;
+pub mod ch05_structs;
+pub mod ch09_errors;
 pub mod ch11_automated_tests;
