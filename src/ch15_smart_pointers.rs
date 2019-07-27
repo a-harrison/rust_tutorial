07f9465da4617a153b7ee5c98857367718531d5d
Adam Harrison <adamaharrison@gmail.com>
2019-07-27 16:31:17 -0700

    Completed 15.4

--- src/ch15_smart_pointers.rs
+++ src/ch15_smart_pointers.rs
@@ -1,1 +1,3 @@
 pub mod main;
+pub mod custom_smart_pointer;
+pub mod reference_counters;
---
+++ src/ch15_smart_pointers/custom_smart_pointer.rs
@@ -0,0 +1,22 @@
+#![allow(unused_variables)]
+#![allow(dead_code)]
+
+struct CustomSmartPointer {
+    data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data `{}`", self.data);
+    }
+}
+
+pub fn main() {
+    let c = CustomSmartPointer { data: String::from("my stuff") };
+    let d = CustomSmartPointer { data: String::from("other stuff") };
+    println!("CustomSmartPointer created.");
+
+    // Manually dropping
+    drop(c);
+    println!("CustomSmartPointer dropped before the end of main.");
+}
